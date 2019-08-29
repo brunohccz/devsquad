dbf3ca78818f03587e3c9c064d47cc042900c828
Bruno Cerciliar <brunocerciliar@gmail.com>
2019-08-29 03:47:10 -0300

    feat: (api) add categories

---
+++ app/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'description'
+    ];
+
+    /**
+     * The mutator will save the slug field automatically.
+     *
+     * @param $value
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
